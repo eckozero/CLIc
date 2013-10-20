268bd2ac1eb11547bd8eb1a16ad90da57d36b84c
EckoZero <lentonp@gmail.com>
2013-10-20 20:05:20 +0100

    Fixed redraw issue

--- CLIc.py
+++ CLIc.py
@@ -81,18 +81,25 @@ class DrawBoard(object):
 			counter1 +=1
 	
 	# FIXME: Redraw doesn't work for anything other than pawns
+	# FIXED: 20/10/13 - ^^ yes it does
 	def redraw_valid(self, valid_move):
 		"""Checks that move is valid, then redraws piece on board"""
 		if valid_move is True:
 			if chess_board[row][column][2] == "P":
 				redraw_valid_for_pawns(valid_move)
-			else: 
-				chess_board[new_row][new_column] = chess_board[row][column]
-				if chess_board[row][column][0] == "{":
-					chess_board[row][column] = "{___}"
+			else:
+				old_piece = chess_board[row][column][0]
+				new_piece = chess_board[row][column][1:4]
+				redraw_piece = chess_board[new_row][new_column][0]
+				if redraw_piece == "{":
+					chess_board[new_row][new_column] = "{" + new_piece + "}"
+				else:
+					chess_board[new_row][new_column] = "(" + new_piece + ")"
+				if old_piece == "{":
+					chess_board[row][column] = "{   }"
 				else:
 					chess_board[row][column] = "(   )"
-
+				
 
 class GameMechanics(object):
 	def __init__(self, turn_counter):
