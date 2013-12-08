e4d26b63293ff91545958a3fd73518c592a609c7
EckoZero <lentonp@gmail.com>
2013-12-08 17:14:53 +0000

    Renamed for consistency

---
+++ src/DrawBoard.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+class DrawBoard(object):
+        def __init__(self, valid_move):
+                self.valid_move = valid_move
+                
+        def print_board(self):
+                """Prints chess board"""
+                counter1 = 0
+                for i in chess_dim:
+                        print "".join(chess_board[counter1])
+                        counter1 +=1
+        
+        # FIXME: Redraw doesn't work for anything other than pawns
+        # FIXED: 20/10/13 - ^^ yes it does
+        def redraw_valid(self, valid_move):
+                """Checks that move is valid, then redraws piece on board"""
+                if piece_colour != chess_board[row][column][1]:
+                        return 0
+                else:
+                        if valid_move is True:
+                                if chess_board[row][column][2] == "P":
+#                                if pawn_capture(row, column, new_row, new_column) == 0:
+                                        redraw_valid_for_pawns(valid_move)
+                                else:
+                                        old_piece = chess_board[row][column][0] 
+                                        new_piece = chess_board[row][column][1:4] 
+                                        redraw_piece = chess_board[new_row][new_column][0] #curly brace
+                                        if redraw_piece == "{": 
+                                                chess_board[new_row][new_column] = "{" + new_piece + "}"
+                                        
+                                        else:
+                                                chess_board[new_row][new_column] = "(" + new_piece + ")"
+                                        if old_piece == "{": 
+                                                chess_board[row][column] = "{___}" 
+                                        else:
+                                                chess_board[row][column] = "(   )"
+
