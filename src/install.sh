fc07505301abd13f006415fb04e65eb8f8936e11
EckoZero <lentonp@gmail.com>
2013-12-08 17:02:01 +0000

    Updated installation script

--- src/install.sh
+++ src/install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 ### Sets up CLIc for use on the command line with the command "CLIc"
 
-if [ -d '/home/`whoami`/.CLIc' ]; then
-    mkdir /home/`whoami`/.CLIc
+if [ -d '/home/`whoami`/.CLIc/src' ]; then
+    mkdir /home/`whoami`/.CLIc/src
 fi
 
-cp CLIc.py /home/`whoami`/.CLIc/CLIc.py && cp README.md /home/`whoami`/.CLIc/README.md
-cp LICENSE /home/`whoami`/.CLIc/LICENSE
+cp CLIc.py /home/`whoami`/.CLIc/CLIc.py && cp ../README.md /home/`whoami`/.CLIc/README.md
+cp ../LICENSE /home/`whoami`/.CLIc/LICENSE
 cp CLIc2.gif /home/`whoami`/.CLIc/CLIc2.gif
 touch /home/`whoami`/.bash_aliases
 
 if [ 'grep -Fq "alias CLIc" /home/`whoami`/.bash_aliases' ]; then
     true
 else
-    echo "alias CLIc='cd /home/`whoami`/.CLIc && python CLIc.py'" >> /home/`whoami`/.bash_aliases
+    echo "alias CLIc='cd /home/`whoami`/.CLIc/src && python CLIc.py'" >> /home/`whoami`/.bash_aliases
 fi
