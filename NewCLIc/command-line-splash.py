2f1dbeed42e643fb2edb8cdfe9dab7fd8498b955
Paul Lenton <lentonp@gmail.com>
2015-10-21 11:47:17 +0100

    Update command-line-splash.py
    Alright. This is definitely feature creep, but I like it.

--- NewCLIc/command-line-splash.py
+++ NewCLIc/command-line-splash.py
@@ -1,3 +1,4 @@
+# Yeah that's supposed to be a King in the ascii. Shut up.
 clic-splash = [
 ["                                                                          "]
 ["                                                      |__|                "],
@@ -11,5 +12,29 @@ clic-splash = [
 ["      \_______|   |_______|   |__|   \_____|    /______________\          "],
 ["                                                                          "],
 ["                      Command     Line    Chess                           "],
+["                                                                          "],
+["                                                                          "],
+["What would you like to do?                                                "],
+["                                                                          "],
+["1. Start new game (vs human)                                              "],
+["2. Start new game (vs CPU)                                                "],
+["3. Load existing game                                                     "],
 ]
+
+# Clearly this isn't going to work, and I'm going to disable it because it will
+# frustrate the hell out of me but whatever
+
+# No selection made so current selection false
+selection_valid = False
+
+while selection_valid != True:
+  game_selection = raw_input("Please make a selection (1, 2, or 3): ")
+
+  if game_selection not in (1,2,3):
+    print "Not a valid selection.\n"
+    print "What would you like to do?\n"
+    print "1. Start new game (vs human)"
+    print "2. Start new game (vs CPU)"
+    print "3. Load existing game"
+
 
